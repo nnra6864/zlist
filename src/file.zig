f6b8ee3dd7e43f846a291798d7848a00f0991c60
Leslie-Lau <804738464@qq.com>
2026-01-27 18:58:49 +0800

    refactor: consolidate size and mtime into stat field, add getPermissions

--- src/file.zig
+++ src/file.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const mem = std.mem;
+const builtin = @import("builtin");
 
 pub const File = struct {
     const Self = @This();
@@ -8,9 +9,7 @@ pub const File = struct {
     is_hidden: bool,
     name: []const u8,
 
-    size: u64,
-    /// Last modification time in nanoseconds, relative to UTC 1970-01-01.
-    mtime: ?std.Io.Timestamp,
+    stat: ?std.Io.Dir.Stat,
 
     pub const Options = struct {
         /// show detail mode
@@ -40,15 +39,13 @@ pub const File = struct {
             .is_dir = is_dir,
             .is_exec = false,
             .name = entry.name,
-            .size = 0,
-            .mtime = null,
+            .stat = null,
         };
 
         if (opt.show_detail) {
             // read more file details
             const stat = try dir.statFile(io, opt.sub_path, .{});
-            file.size = stat.size;
-            file.mtime = stat.mtime;
+            file.stat = stat;
         }
 
         return file;
@@ -78,5 +75,52 @@ pub const File = struct {
             // default file color
             return Color.light_yellow;
         }
+    }
+
+    pub inline fn getPermissions(self: Self, buf: *[10]u8) []const u8 {
+        if (!self.stat) {
+            // unknown permissions
+            @memset(buf, '-');
+            return buf[0..10];
+        }
+
+        buf[0] = switch (self.stat.?.kind) {
+            // block_device,
+            .directory => 'd',
+            .sym_link => 'l',
+            .unix_domain_socket => 's',
+            .character_device => 'c',
+            else => '-', // regular file
+        };
+
+        if (builtin.os.tag == .windows) {
+            // TODO leslie: todo
+        } else {
+            // posix permissions
+            const m = @intFromEnum(self.stat.?.permissions);
+            // Permission bits from posix standard
+            const S_IRUSR = 0o400;
+            const S_IWUSR = 0o200;
+            const S_IXUSR = 0o100;
+            const S_IRGRP = 0o040;
+            const S_IWGRP = 0o020;
+            const S_IXGRP = 0o010;
+            const S_IROTH = 0o004;
+            const S_IWOTH = 0o002;
+            const S_IXOTH = 0o001;
+            // User
+            buf[1] = if (m & S_IRUSR != 0) 'r' else '-';
+            buf[2] = if (m & S_IWUSR != 0) 'w' else '-';
+            buf[3] = if (m & S_IXUSR != 0) 'x' else '-';
+            // Group
+            buf[4] = if (m & S_IRGRP != 0) 'r' else '-';
+            buf[5] = if (m & S_IWGRP != 0) 'w' else '-';
+            buf[6] = if (m & S_IXGRP != 0) 'x' else '-';
+            // Other
+            buf[7] = if (m & S_IROTH != 0) 'r' else '-';
+            buf[8] = if (m & S_IWOTH != 0) 'w' else '-';
+            buf[9] = if (m & S_IXOTH != 0) 'x' else '-';
+        }
+        return buf;
     }
 };
